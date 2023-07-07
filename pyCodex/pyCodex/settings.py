17ff2202cbc9e10ccf4af347cb7a5dd711d1ec64
Anna <annaaraqelyan140293@gmail.com>
2023-07-07 15:19:24 +0400

    Header nav bar is ready

--- pyCodex/pyCodex/settings.py
+++ pyCodex/pyCodex/settings.py
@@ -119,8 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
