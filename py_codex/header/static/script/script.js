78de3ed250bba1add4d8bb8924eafad6514a608f
Prostoyjitel <xelzim.rubina.2021@gmail.com>
2023-07-10 19:48:41 +0400

    feat: added response py-codex and bnt-section sections

--- py_codex/header/static/script/script.js
+++ py_codex/header/static/script/script.js
@@ -2,7 +2,7 @@ const menuBar = document.getElementById("menu-bars");
 const navBar = document.querySelector(".header-content-navbar");
 
 const openMenu = () => {
-    if (navBar.classList[1] === 'active') navBar.classList.remove("active");
+    if (navBar.classList[1] === "active") navBar.classList.remove("active");
     else {
         navBar.classList.add("active");
     }
